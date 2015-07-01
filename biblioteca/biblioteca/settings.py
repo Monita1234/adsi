1b22ae90081da2447d8ef78587e6c1c61241a97d
Monita1234 <nayibemontealegre@hotmail.com>
2015-07-01 18:39:48 -0500

    set
    add

--- biblioteca/biblioteca/settings.py
+++ biblioteca/biblioteca/settings.py
@@ -23,7 +23,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['biblioadsi.herokuapp.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -128,7 +128,7 @@ INSTALLED_APPS = (
     'biblioteca.apps.libros',
     'biblioteca.apps.reportes',
     'django.contrib.humanize'
-    #'gunicorn'
+    'gunicorn'
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
